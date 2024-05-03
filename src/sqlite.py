9b92ff211cca44869e1d06617133723de98da73f
CarlosAMolina <15368012+CarlosAMolina@users.noreply.github.com>
2024-05-03 14:30:34 +0200

    Change use connection instead of cursor

--- src/sqlite.py
+++ src/sqlite.py
@@ -6,15 +6,15 @@ def main():
     db_file = "/tmp/contacts.test.sqlite3"
     connection = sqlite3.connect(db_file)
     cursor = connection.cursor()
-    table_names = _get_table_names(cursor)
+    table_names = _get_table_names(connection)
     for index, table_name in enumerate(table_names, 1):
         print(f"{index}/{len(table_names)}", table_name)
         rows = _get_table_data(cursor, table_name)
         print(rows)
 
 
-def _get_table_names(cursor: sqlite3.Cursor) -> tp.List[str]:
-    response = cursor.execute("SELECT * FROM sqlite_master where type='table'")
+def _get_table_names(connection: sqlite3.Connection) -> tp.List[str]:
+    response = connection.execute("SELECT * FROM sqlite_master where type='table'")
     rows = response.fetchall()
     result = []
     for row in rows:
