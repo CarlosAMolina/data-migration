9f787c24b9cb69109e920845120185a90f134b04
CarlosAMolina <15368012+CarlosAMolina@users.noreply.github.com>
2024-05-03 14:22:55 +0200

    Refactor rename

--- src/sqlite.py
+++ src/sqlite.py
@@ -1,15 +1,17 @@
 import sqlite3
 import typing as tp
 
+
 def main():
     db_file = "/tmp/contacts.test.sqlite3"
     connection = sqlite3.connect(db_file)
     cursor = connection.cursor()
-    table_names = _get_tables(cursor)
+    table_names = _get_table_names(cursor)
     for index, table_name in enumerate(table_names, 1):
         print(f"{index}/{len(table_names)}", table_name)
 
-def _get_tables(cursor: sqlite3.Cursor) -> tp.List[str]:
+
+def _get_table_names(cursor: sqlite3.Cursor) -> tp.List[str]:
     response = cursor.execute("SELECT * FROM sqlite_master where type='table'")
     rows = response.fetchall()
     result = []
@@ -17,6 +19,7 @@ def _get_tables(cursor: sqlite3.Cursor) -> tp.List[str]:
         table_name = row[1]
         result.append(table_name)
     return result
+
 
 if __name__ == "__main__":
     main()
