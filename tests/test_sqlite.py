e1a5de0c7d12b94563f99508ba6a4103b569fe2a
CarlosAMolina <15368012+CarlosAMolina@users.noreply.github.com>
2024-05-03 14:36:55 +0200

    Update test

--- tests/test_sqlite.py
+++ tests/test_sqlite.py
@@ -9,6 +9,7 @@ class TestFunctions(unittest.TestCase):
     def test_get_table_names(self):
         con = sqlite3.connect(":memory:")
         con.execute("CREATE TABLE lang(id INTEGER PRIMARY KEY, name VARCHAR UNIQUE)")
+        con.execute("CREATE TABLE lang_2(id INTEGER PRIMARY KEY, name VARCHAR UNIQUE)")
         result = sqlite._get_table_names(con)
-        expected_result = ["lang"]
+        expected_result = ["lang", "lang_2"]
         self.assertEqual(expected_result, result)
