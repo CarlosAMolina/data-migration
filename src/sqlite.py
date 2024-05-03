72dac4c12f5882e413887b90c54603f30f759901
CarlosAMolina <15368012+CarlosAMolina@users.noreply.github.com>
2024-05-03 11:47:20 +0200

    Add sqlite get table names

---
+++ src/sqlite.py
@@ -0,0 +1,22 @@
+import sqlite3
+import typing as tp
+
+def main():
+    db_file = "/tmp/contacts.test.sqlite3"
+    connection = sqlite3.connect(db_file)
+    cursor = connection.cursor()
+    table_names = get_tables(cursor)
+    for index, table_name in enumerate(table_names, 1):
+        print(f"{index}/{len(table_names)}", table_name)
+
+def get_tables(cursor: sqlite3.Cursor) -> tp.List[str]:
+    response = cursor.execute("SELECT * FROM sqlite_master where type='table'")
+    rows = response.fetchall()
+    result = []
+    for row in rows:
+        table_name = row[1]
+        result.append(table_name)
+    return result
+
+if __name__ == "__main__":
+    main()
