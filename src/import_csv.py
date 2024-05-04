966d39216e31b33fadeb7c834eea495b2b2c5f47
CarlosAMolina <15368012+CarlosAMolina@users.noreply.github.com>
2024-05-04 14:05:57 +0200

    Add get table name from csv

--- src/import_csv.py
+++ src/import_csv.py
@@ -5,8 +5,11 @@ from pathlib import Path
 
 def import_csv_directory(csv_directory_path_name: str, db_file_path_name: str):
     print("Start importing", csv_directory_path_name, "files to", db_file_path_name)
-    csv_file_path_names = _get_csv_file_path_names_in_directory()(csv_directory_path_name)
-    print(csv_file_path_names)
+    csv_file_path_names = _get_csv_file_path_names_in_directory(csv_directory_path_name)
+    for index, csv_file_path_name in enumerate(csv_file_path_names, 1):
+        table_name = _get_table_name_from_csv_file_path_name(csv_file_path_name)
+        print("Start table", f"{index}/{len(csv_file_path_names)}", table_name)
+        print("Importing", csv_file_path_name)
 
 
 def _get_csv_file_path_names_in_directory(directory_path_name: str) -> tp.List[str]:
@@ -16,4 +19,5 @@ def _get_csv_file_path_names_in_directory(directory_path_name: str) -> tp.List[s
     return result
 
 
-# def _get_table_name_from_csv_fi
+def _get_table_name_from_csv_file_path_name(csv_file_path_name: str) -> str:
+    return Path(csv_file_path_name).stem
