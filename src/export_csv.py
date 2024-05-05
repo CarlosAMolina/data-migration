42fad9fdad8c08df95e68b65cd660aeb6f859895
CarlosAMolina <15368012+CarlosAMolina@users.noreply.github.com>
2024-05-05 13:26:18 +0200

    Refactor extact function

--- src/export_csv.py
+++ src/export_csv.py
@@ -13,27 +13,21 @@ def export_to_csv(db_file_path_name: str):
     print("Start exporting SQLite database:", db_file_path_name)
     if not Path(db_file_path_name).exists():
         raise FileExistsError(db_file_path_name)
-    directory_export_path_name = _SQLiteDirectoryNameGenerator().get_directory_path_name(db_file_path_name)
-    print("Start creating directory:", directory_export_path_name)
-    Path(directory_export_path_name).mkdir()
     db = SQLiteDatabase(db_file_path_name=db_file_path_name)
-    table_names = db.get_table_names()
-    for index, table_name in enumerate(table_names, 1):
-        print(f"Start table {index}/{len(table_names)}", table_name)
-        rows = db.get_table_data(table_name)
-        colum_names = db.get_table_column_names(table_name)
-        csv_file_path_name = "{}/{}.csv".format(directory_export_path_name, table_name)
-        print("Exporting table to", csv_file_path_name)
-        _export_rows_to_csv(colum_names, rows, csv_file_path_name)
+    directory_export_path_name = _SQLiteDirectoryNameGenerator().get_directory_path_name(db_file_path_name)
+    _export_to_csv(db, directory_export_path_name)
 
 
-# TODO refactor duplicated code in export_to_csv
 def export_to_csv_postgresql():
     print("Start exporting PostgreSQL database")
     directory_export_path_name = _PostgreSQLDirectoryNameGenerator().get_directory_path_name(POSTGRESQL_SCHEMA)
+    db = PostgreSQLDatabase()
+    _export_to_csv(db, directory_export_path_name)
+
+
+def _export_to_csv(db: tp.Union[SQLiteDatabase, PostgreSQLDatabase], directory_export_path_name: str):
     print("Start creating directory:", directory_export_path_name)
     Path(directory_export_path_name).mkdir()
-    db = PostgreSQLDatabase()
     table_names = db.get_table_names()
     for index, table_name in enumerate(table_names, 1):
         print(f"Start table {index}/{len(table_names)}", table_name)
