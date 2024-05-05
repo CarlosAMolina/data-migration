c61cb4181143780bc120483cd33b6690825679a2
CarlosAMolina <15368012+CarlosAMolina@users.noreply.github.com>
2024-05-05 13:28:31 +0200

    Drop unrequired code

--- src/main.py
+++ src/main.py
@@ -1,8 +1,8 @@
 import sys
 
-from database_postgresql import PostgreSQLDatabase  # TODO rm
 from export_csv import export_to_csv
-from export_csv import export_to_csv_postgresql
+
+# TODO from export_csv import export_to_csv_postgresql
 from import_csv import import_csv_directory
 
 
@@ -19,11 +19,4 @@ def main():
 
 
 if __name__ == "__main__":
-    # main() # TODO uncomment
-    # TODO drop below
-    db = PostgreSQLDatabase()
-    print(db.get_table_names())
-    print(db.get_table_data("addresses"))
-    print(db.get_table_column_names("addresses"))
-    export_to_csv_postgresql()
-    # db.test()
+    main()
