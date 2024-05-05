414001f135eb16d84e7dde3eed3ccecb1323d660
CarlosAMolina <15368012+CarlosAMolina@users.noreply.github.com>
2024-05-05 13:39:10 +0200

    Fix compare enum

--- src/main.py
+++ src/main.py
@@ -17,7 +17,7 @@ def main():
     if len(sys.argv) < 2:
         _raise_incorrect_args()
     db_type = sys.argv[1]
-    if db_type == DB_TYPE.SQLITE:
+    if db_type == DB_TYPE.SQLITE.value:
         if len(sys.argv) == 3:
             db_file_path_name = sys.argv[2]
             export_to_csv(db_file_path_name)
@@ -27,7 +27,7 @@ def main():
             import_csv_directory(csv_directory_path_name, db_file_path_name)
         else:
             _raise_incorrect_args()
-    elif db_type == DB_TYPE.POSTGRESQL:
+    elif db_type == DB_TYPE.POSTGRESQL.value:
         export_to_csv_postgresql()
     else:
         _raise_incorrect_args()
