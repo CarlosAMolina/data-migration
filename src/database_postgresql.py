eb4ba5729df9de0fab7e7b5f07aaa18a11618827
CarlosAMolina <15368012+CarlosAMolina@users.noreply.github.com>
2024-05-05 14:04:01 +0200

    Fix get postgresql column names order. Refactor extract variables

--- src/database_postgresql.py
+++ src/database_postgresql.py
@@ -18,11 +18,12 @@ class PostgreSQLDatabase:
 
     def get_table_names(self) -> tp.List[str]:
         with psycopg.connect(self._uri) as conn:
-            rows = conn.execute(
+            response = conn.execute(
                 "SELECT * FROM pg_catalog.pg_tables"
                 " WHERE schemaname != 'pg_catalog' AND"
                 " schemaname != 'information_schema';"
-            ).fetchall()
+            )
+            rows = response.fetchall()
         result = []
         for row in rows:
             table_name = row[1]
@@ -30,13 +31,18 @@ class PostgreSQLDatabase:
         return result
 
     def get_table_data(self, table_name: str) -> Rows:
+        query = f"SELECT * FROM {config.SCHEMA}.{table_name}"
         with psycopg.connect(self._uri) as conn:
-            return conn.execute(f"SELECT * FROM {config.SCHEMA}.{table_name}").fetchall()
+            response = conn.execute(query)
+            rows = response.fetchall()
+        return rows
 
     def get_table_column_names(self, table_name: str) -> Rows:
+        query = (
+            f"SELECT column_name FROM information_schema.columns WHERE table_name = '{table_name}'"
+            " order by ordinal_position ASC;"
+        )
         with psycopg.connect(self._uri) as conn:
-            rows = conn.execute(
-                f"SELECT column_name FROM information_schema.columns WHERE table_name = '{table_name}';"
-            ).fetchall()
+            rows = conn.execute(query).fetchall()
         result = [row[0] for row in rows]
         return result
