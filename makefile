58740b96d7b2e0a4ffb03cfecfefb0cc2beddfb1
CarlosAMolina <15368012+CarlosAMolina@users.noreply.github.com>
2024-05-05 13:35:04 +0200

    Update main to work with SQLite and PostgreSQL db

--- makefile
+++ makefile
@@ -1,8 +1,11 @@
+export-postgresql:
+	python src/main.py postgresql
+
 export-sqlite:
-	python src/main.py /tmp/contacts.sqlite3
+	python src/main.py sqlite /tmp/contacts.sqlite3
 
 import-sqlite:
-	python src/main.py /tmp/contacts.sqlite3-20240504131728/ /tmp/contacts.sqlite3
+	python src/main.py sqlite /tmp/contacts.sqlite3-20240504131728/ /tmp/contacts.sqlite3
 
 test:
 	python -m unittest discover -s tests
--- src/main.py
+++ src/main.py
@@ -2,20 +2,39 @@ import sys
 
 from export_csv import export_to_csv
 
-# TODO from export_csv import export_to_csv_postgresql
+from export_csv import export_to_csv_postgresql
 from import_csv import import_csv_directory
+
+from enum import Enum
+
+
+class DB_TYPE(Enum):
+    SQLITE = "sqlite"
+    POSTGRESQL = "postgresql"
 
 
 def main():
-    if len(sys.argv) == 2:
-        db_file_path_name = sys.argv[1]
-        export_to_csv(db_file_path_name)
-    elif len(sys.argv) == 3:
-        csv_directory_path_name = sys.argv[1]
-        db_file_path_name = sys.argv[2]
-        import_csv_directory(csv_directory_path_name, db_file_path_name)
+    if len(sys.argv) < 2:
+        _raise_incorrect_args()
+    db_type = sys.argv[1]
+    if db_type == DB_TYPE.SQLITE:
+        if len(sys.argv) == 3:
+            db_file_path_name = sys.argv[2]
+            export_to_csv(db_file_path_name)
+        elif len(sys.argv) == 4:
+            csv_directory_path_name = sys.argv[2]
+            db_file_path_name = sys.argv[3]
+            import_csv_directory(csv_directory_path_name, db_file_path_name)
+        else:
+            _raise_incorrect_args()
+    elif db_type == DB_TYPE.POSTGRESQL:
+        export_to_csv_postgresql()
     else:
-        raise ValueError("Incorrect args")
+        _raise_incorrect_args()
+
+
+def _raise_incorrect_args():
+    raise ValueError("Incorrect args")
 
 
 if __name__ == "__main__":
